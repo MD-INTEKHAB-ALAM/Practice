78fe786329b88368daa412aa957bf13060ce866b
Md Intekhab Alam <intekhab8928@gmail.com>
2025-12-26 19:12:19 +0530

    added userId to the request in jwtmiddleware

--- backend/mongodb/middlewares/jwt.middleware.js
+++ backend/mongodb/middlewares/jwt.middleware.js
@@ -2,12 +2,13 @@ import jwt from "jsonwebtoken";
 
 export default function jwtAuth(req,res,next) {
     const token = req.headers["authorization"];
-    if(!authHeader) {
+    if(!token) {
         return res.status(401).send("Header is empty");
     }
 
     try {
         const payload = jwt.verify(token,"3dda83f28c1fd9ca6ff0111c82865bfc");
+        req.userID = payload.userID;
     }
     catch(err) {
         return res.status(401).send("Unauthorized");
